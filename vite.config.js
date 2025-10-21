08bce1b772ec2727d8c7bb906a8cb95224d744d0
Lord-Gabe <gabolusesan@gmail.com>
2025-10-21 11:40:33 +0100

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  eslint: { 
+    // Enable ESLint during development and build
+    enable: true,
+    ignoreDuringBuilds: false
+  }
 })
